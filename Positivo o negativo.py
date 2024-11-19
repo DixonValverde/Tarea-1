dd6e1268f31339fc1dcba595064f5253b1f823c4
boris <borisvalverde8@gmail.com>
2024-11-19 15:03:13 -0500

    cambios en los archivos

---
+++ Positivo o negativo.py
@@ -0,0 +1,12 @@
+#o	Solicita al usuario un número y determina si es positivo, negativo o cero.
+#	Ejemplo: Entrada: -3 → Salida: "Es un número negativo".
+
+
+nume=(int(input("ingresa un numero positivo o negativo: ")))
+
+if nume> 0:
+    print("Es positivo")
+elif nume < 0:
+    print("Es negativo")
+else:
+    print("el numero es cero")
---
+++ mayor o menor.py
@@ -0,0 +1,11 @@
+#	Escribe un programa que solicite un número y determine si es mayor o menor que 10.
+#	Ejemplo: Entrada: 5 → Salida: "Es menor que 10".
+nume = (int(input("porfavor escribir un numero entero: ")))
+  
+
+if nume > 10:
+    print("Es mayor que 10")
+else:
+    print("Es menor o igual que 10")
+
+#realizado por BDVA 
