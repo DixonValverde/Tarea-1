b3cb84c944cd37f9b84cf430af7327a6f686b5df
boris <borisvalverde8@gmail.com>
2024-11-19 16:49:10 -0500

    archivo agregado

---
+++ Resultado de calificaciones.py
@@ -0,0 +1,12 @@
+#	Aprobado o reprobado:
+#	Solicita la calificación de un estudiante y determina si está aprobado (mayor o igual a 7) o reprobado.
+#	Ejemplo: Entrada: 6.5 → Salida: "Reprobado".
+nume = (float(input("porfavor escribir la calificacion: ")))
+  
+
+if nume > 7:
+    print("Estas aprobado!!")
+else:
+    print("Reprobado A ESTUDIAR MAS!!")
+
+#realizado por BDVA
